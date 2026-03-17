ee5b00132bff415f31989715aff4518e2c31b841
mrogenki <mr.ogenki@gmail.com>
2026-03-18 07:30:04 +0800

    feat: Adjust hero image aspect ratio to video
    Changes the aspect ratio of the main hero image on the home page from square to video (16:9). This provides a more appropriate display for featured promotional content.

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -218,7 +218,7 @@ const Home: React.FC<HomeProps> = ({ activities, memberActivities, clubActivitie
                     </div>
                   )}
                 </div>
-                <div className="relative aspect-square lg:aspect-auto overflow-hidden">
+                <div className="relative aspect-video overflow-hidden">
                   <AnimatePresence mode="wait">
                     <motion.img
                       key={clubActivities[clubSlide].id}
